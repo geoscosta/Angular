bb2191a43890b083047fb3846aa93c8e53a290ab
Geovane Silva <geoscosta@gmail.com>
2023-04-13 23:05:06 -0300

    Exibição de mapas

--- src/app/fornecedor/detalhes/detalhes.component.ts
+++ src/app/fornecedor/detalhes/detalhes.component.ts
@@ -3,6 +3,7 @@ import { Fornecedor } from '../models/fornecedor';
 
 import { ActivatedRoute } from '@angular/router';
 import { FornecedorService } from '../services/fornecedor.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detalhes',
@@ -10,8 +11,17 @@ import { FornecedorService } from '../services/fornecedor.service';
 })
 export class DetalhesComponent {
   fornecedor: Fornecedor = new Fornecedor();
+  enderecoMap;
 
-  constructor(private route: ActivatedRoute) {
-    this.fornecedor = this.route.snapshot.data['fornecedor'];
+  constructor(
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer) {
+
+      this.fornecedor = this.route.snapshot.data['fornecedor'];
+      this.enderecoMap = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.google.com/maps/embed/v1/place?q=" + this.EnderecoCompleto() + "&key=AIzaSyAP0WKpL7uTRHGKWyakgQXbW6FUhrrA5pE");
+  }
+
+  public EnderecoCompleto(): string {
+    return this.fornecedor.endereco.logradouro + ", " + this.fornecedor.endereco.numero + " - " + this.fornecedor.endereco.bairro + ", " + this.fornecedor.endereco.cidade + " - " + this.fornecedor.endereco.estado;
   }
 }

c2fe4d6249a88a8e714281cf8f91c9a34bbf3842
Geovane Silva <geoscosta@gmail.com>
2023-04-14 18:35:35 -0300

    Forma alternativa de upload de arquivos

--- src/app/produto/editar/editar.component.ts
+++ src/app/produto/editar/editar.component.ts
@@ -12,6 +12,7 @@ import { ValidationMessages, GenericValidator, DisplayMessage } from 'src/app/ut
 import { Produto, Fornecedor } from '../models/produto';
 import { ProdutoService } from '../services/produto.service';
 import { environment } from 'src/environments/environment';
+import { CurrencyUtils } from 'src/app/utils/currency-utils';
 
 @Component({
   selector: 'app-editar',
@@ -22,6 +23,11 @@ export class EditarComponent implements OnInit {
   imagens: string = environment.imagensUrl;
 
   @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
+
+  imageBase64: any;
+  imagemPreview: any;
+  imagemNome: string;
+  imagemOriginalSrc: string;
 
   produto: Produto;
   fornecedores: Fornecedor[];
@@ -90,8 +96,11 @@ export class EditarComponent implements OnInit {
       nome: this.produto.nome,
       descricao: this.produto.descricao,
       ativo: this.produto.ativo,
-      valor: this.produto.valor
+      valor: CurrencyUtils.DecimalParaString(this.produto.valor)
     });
+
+    // utilizar o [src] na imagem para evitar que se perca após post
+    this.imagemOriginalSrc = this.imagens + this.produto.imagem;
   }
 
   ngAfterViewInit(): void {
@@ -107,6 +116,13 @@ export class EditarComponent implements OnInit {
   editarProduto() {
     if (this.produtoForm.dirty && this.produtoForm.valid) {
       this.produto = Object.assign({}, this.produto, this.produtoForm.value);
+
+      if (this.imageBase64) {
+        this.produto.imagemUpload = this.imageBase64;
+        this.produto.imagem = this.imagemNome;
+      }
+
+      this.produto.valor = CurrencyUtils.StringParaDecimal(this.produto.valor);
 
      this.produtoService.atualizarProduto(this.produto)
         .subscribe(
@@ -133,6 +149,20 @@ export class EditarComponent implements OnInit {
   processarFalha(fail: any) {
     this.errors = fail.error.errors;
     this.toastr.error('Ocorreu um erro!', 'Opa :(');
+  }
+
+  upload(file: any) {
+    this.imagemNome = file[0].name;
+
+    var reader = new FileReader();
+    reader.onload = this.manipularReader.bind(this);
+    reader.readAsBinaryString(file[0]);
+  }
+
+  manipularReader(readerEvt: any) {
+    var binaryString = readerEvt.target.result;
+    this.imageBase64 = btoa(binaryString);
+    this.imagemPreview = "data:image/jpeg;base64," + this.imageBase64;
   }
 }
 

2f113a351342c64c75512e0988bef4e1eac96b60
Geovane Silva <geoscosta@gmail.com>
2023-05-25 20:12:16 -0300

    Refatorando serviços de guarda

--- src/app/fornecedor/services/fornecedor.guard.ts
+++ src/app/fornecedor/services/fornecedor.guard.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanDeactivate } from '@angular/router';
-import { LocalStorageUtils } from 'src/app/utils/localstorage';
+import { ActivatedRouteSnapshot, CanActivate, CanDeactivate, Router } from '@angular/router';
+import { BaseGuard } from 'src/app/services/base.guard';
+
 import { NovoComponent } from '../novo/novo.component';
 
 @Injectable()
-export class FornececedorGuard implements CanActivate, CanDeactivate<NovoComponent> {
+export class FornececedorGuard extends BaseGuard implements CanActivate, CanDeactivate<NovoComponent> {
 
-    localStorageUtils = new LocalStorageUtils();
-
-    constructor(private router: Router) { }
+  constructor(protected override router: Router){ super(router); }
 
     canDeactivate(component: NovoComponent) {
         if(component.mudancasNaoSalvas) {
@@ -17,41 +16,7 @@ export class FornececedorGuard implements CanActivate, CanDeactivate<NovoCompone
         return true
     }
 
-    canActivate(routeAc: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-
-        if (!this.localStorageUtils.obterTokenUsuario()) {
-            this.router.navigate(['/conta/login'], {queryParams: { returnUrl: this.router.url}});
-        }
-
-        let user = this.localStorageUtils.obterUsuario();
-        let claim: any = routeAc.data[0];
-
-        if (claim !== undefined) {
-            let claim = routeAc.data[0]['claim'];
-
-            if (claim) {
-                if (!user.claims) {
-                    this.navegarAcessoNegado();
-                }
-
-                let userClaims = user.claims.find(x => x.type === claim.nome);
-
-                if (!userClaims) {
-                    this.navegarAcessoNegado();
-                }
-
-                let valoresClaim = userClaims.value as string;
-
-                if (!valoresClaim.includes(claim.valor)) {
-                    this.navegarAcessoNegado();
-                }
-            }
-        }
-
-        return true;
-    }
-
-    navegarAcessoNegado() {
-        this.router.navigate(['/acesso-negado']);
-    }
+    canActivate(routeAc: ActivatedRouteSnapshot) {
+      return super.validarClaims(routeAc);
+  }
 }
--- src/app/produto/services/produto.guard.ts
+++ src/app/produto/services/produto.guard.ts
@@ -1,57 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanDeactivate, Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanDeactivate, Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 
-import { LocalStorageUtils } from 'src/app/utils/localstorage';
+
 import { NovoComponent } from '../novo/novo.component';
+import { BaseGuard } from 'src/app/services/base.guard';
 
 @Injectable()
-export class ProdutoGuard implements CanActivate, CanDeactivate<NovoComponent> {
-    
-    localStorageUtils = new LocalStorageUtils();
+export class ProdutoGuard extends BaseGuard implements CanActivate, CanDeactivate<NovoComponent> {
 
-    constructor(private router: Router){}
+    constructor(protected override router: Router){ super(router); }
 
     canDeactivate(component: NovoComponent) {
         if(component.mudancasNaoSalvas) {
             return window.confirm('Tem certeza que deseja abandonar o preenchimento do formulario?');
-        }        
+        }
         return true
     }
 
-    canActivate(routeAc: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if(!this.localStorageUtils.obterTokenUsuario()){
-            this.router.navigate(['/conta/login/'], { queryParams: { returnUrl: this.router.url }});
-        }  
-
-        let user = this.localStorageUtils.obterUsuario();
-
-        let claim: any = routeAc.data[0];
-        if (claim !== undefined) {
-            let claim = routeAc.data[0]['claim'];
-
-            if (claim) {
-                if (!user.claims) {
-                    this.navegarAcessoNegado();
-                }
-                
-                let userClaims = user.claims.find(x => x.type === claim.nome);
-                
-                if(!userClaims){
-                    this.navegarAcessoNegado();
-                }
-                
-                let valoresClaim = userClaims.value as string;
-
-                if (!valoresClaim.includes(claim.valor)) {
-                    this.navegarAcessoNegado();
-                }
-            }
-        }
-
-        return true;  
-    }
-
-    navegarAcessoNegado() {
-        this.router.navigate(['/acesso-negado']);
+    canActivate(routeAc: ActivatedRouteSnapshot) {
+        return super.validarClaims(routeAc);
     }
 }
